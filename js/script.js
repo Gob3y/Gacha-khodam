b5418730a89f71cfd3e6dc87c7c0410a0dce212e
Gob3y <Galihjohan79@gmail.com>
2024-06-15 20:45:46 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.getElementById('kodamForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+
+    fetch('/khodam/khodam.txt')
+        .then(response => response.text())
+        .then(data => {
+            const kodams = data.split('\n').map(kodam => kodam.trim()).filter(kodam => kodam);
+            const randomKodam = kodams[Math.floor(Math.random() * kodams.length)];
+            const resultElement = document.getElementById('result');
+            resultElement.innerText = `Nama: ${name}, Kodam: ${randomKodam}`;
+            resultElement.classList.add('show');
+        })
+        .catch(error => {
+            console.error('Error fetching the kodam list:', error);
+            const resultElement = document.getElementById('result');
+            resultElement.innerText = 'Error fetching the kodam list.';
+            resultElement.classList.add('show');
+        });
+});
