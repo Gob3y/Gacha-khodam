3650f8073e8b49c7225fc851f4e33645b08355d7
Gob3y <Galihjohan79@gmail.com>
2024-06-21 00:17:41 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ document.getElementById('kodamForm').addEventListener('submit', function(event) 
             const kodams = data.split('\n').map(kodam => kodam.trim()).filter(kodam => kodam);
             const randomKodam = kodams[Math.floor(Math.random() * kodams.length)];
             const resultElement = document.getElementById('result');
-            resultElement.innerText = Nama : ${name}\nKhodam : ${randomKodam};
+            resultElement.innerText = `Nama: ${name}\nKhodam: ${randomKodam}`;
             resultElement.classList.add('show');
 
             const tableBody = document.getElementById('checkedTableBody');
